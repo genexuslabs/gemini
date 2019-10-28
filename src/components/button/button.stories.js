20b702639ebb36ba82b7a7bd9e26047432691e6a
bsastre <bsastre@genexus.com>
2019-10-28 15:19:35 -0300

    Add Icons knob

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,30 +1,46 @@
 import { storiesOf } from '@storybook/html';
 import readme from "./readme.md";
-import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
+import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs';
+
+/*************
+KNOBS
+*************/
+
+/*Icons Knob*/
+const label = 'Icon';
+const options = {
+  Add: './assets/icons/gxg-icon-add.svg',
+  ArrowDown: './assets/icons/gxg-icon-arrow-down.svg',
+};
+const defaultValue = options.Add;
+
+/*Disabled Knob*/
+const labelDisabled = 'Disabled';
+const defaultValueDisabled = false;
 
 
+/*************
+STORIES
+*************/
 const stories = storiesOf("Button", module);
 stories.addDecorator(withKnobs);
 // storiesOf('Button', module)
 stories.add('Primary - Text', () => `
-        <gxg-button type="primary">${text("Button Label","Button")}</gxg-button>
-        <gxg-button type="primary" disabled>${text("Button Label","Button")}</gxg-button>
+        <gxg-button type="primary" disabled=${boolean(labelDisabled, defaultValueDisabled)}> ${text("Button Label","Button")}</gxg-button>
         `, {
         notes: {
                 markdown: readme
         }
 })
 .add('Primary - Text / Icon', () => `
-        <gxg-button><img slot="icon" src="./assets/icons/gxg-icon-add.svg">${text("Button Label","Button")}</gxg-button>
-        <gxg-button disabled><img slot="icon" src="./assets/icons/gxg-icon-add.svg">${text("Button Label","Button")}</gxg-button>
+        <gxg-button disabled=${boolean(labelDisabled, defaultValueDisabled)}><img slot="icon" src="${select(label, options, defaultValue)}">${text("Button Label","Button")}</gxg-button>
         `, {
         notes: {
                 markdown: readme
         }
 })
 .add('Primary - Icon', () => `
-        <gxg-button type="icon-only"><img slot="icon" src="./assets/icons/gxg-icon-add.svg"></gxg-button>
-        <gxg-button type="icon-only" disabled><img slot="icon" src="./assets/icons/gxg-icon-add.svg"></gxg-button>
+        <gxg-button type="icon-only" disabled=${boolean(labelDisabled, defaultValueDisabled)}><img slot="icon" src="${select(label, options, defaultValue)}"></gxg-button>
         `
         , {
         notes: {
@@ -32,24 +48,21 @@ stories.add('Primary - Text', () => `
         }
 })
 .add('Secondary - Text', () => `
-        <gxg-button type="text-only">${text("Button Label","Button")}</gxg-button>
-        <gxg-button type="text-only" disabled>${text("Button Label","Button")}</gxg-button>
+        <gxg-button type="text-only" disabled=${boolean(labelDisabled, defaultValueDisabled)}>${text("Button Label","Button")}</gxg-button>
         `, {
         notes: {
                markdown: readme
         }
 })
 .add('Secondary - Icon', () => `
-        <gxg-button type="icon-only-secondary"><img slot="icon" src="./assets/icons/gxg-icon-add.svg"></gxg-button>
-        <gxg-button type="icon-only-secondary" disabled><img slot="icon" src="./assets/icons/gxg-icon-add.svg"></gxg-button>
+        <gxg-button type="icon-only-secondary" disabled=${boolean(labelDisabled, defaultValueDisabled)}><img slot="icon" src="${select(label, options, defaultValue)}"></gxg-button>
         `, {
         notes: {
                markdown: readme
         }
 })
 .add('Outlined', () => `
-        <gxg-button type="outlined">${text("Button Label","Button")}</gxg-button>
-        <gxg-button type="outlined" disabled>${text("Button Label","Button")}</gxg-button>
+        <gxg-button type="outlined" disabled=${boolean(labelDisabled, defaultValueDisabled)}>${text("Button Label","Button")}</gxg-button>
         `, {
         notes: {
                 markdown: readme
