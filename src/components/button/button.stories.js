aea84a0ac516ec3b7661ce1b4b2fc083d725397a
bsastre <bsastre@genexus.com>
2019-11-01 16:48:38 -0300

    Add knobs to button stories

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,7 +1,6 @@
 import { storiesOf } from '@storybook/html';
 import readme from "./readme.md";
 import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs';
-import { withA11y } from '@storybook/addon-a11y';
 
 /*************
 KNOBS
@@ -25,7 +24,6 @@ STORIES
 *************/
 const stories = storiesOf("Button", module);
 stories.addDecorator(withKnobs);
-stories.addDecorator(withA11y);
 // storiesOf('Button', module)
 stories.add('Primary - Text', () => `
         <gxg-button type="primary" disabled=${boolean(labelDisabled, defaultValueDisabled)}> ${text("Button Label","Button")}</gxg-button>
@@ -35,14 +33,14 @@ stories.add('Primary - Text', () => `
         }
 })
 .add('Primary - Text / Icon', () => `
-        <gxg-button disabled=${boolean(labelDisabled, defaultValueDisabled)}><img slot="icon" src="${select(label, options, defaultValue)}" alt="icon">${text("Button Label","Button")}</gxg-button>
+        <gxg-button disabled=${boolean(labelDisabled, defaultValueDisabled)}><img slot="icon" src="${select(label, options, defaultValue)}">${text("Button Label","Button")}</gxg-button>
         `, {
         notes: {
                 markdown: readme
         }
 })
 .add('Primary - Icon', () => `
-        <gxg-button type="icon-only" disabled=${boolean(labelDisabled, defaultValueDisabled)}><img slot="icon" src="${select(label, options, defaultValue)}" alt="icon"></gxg-button>
+        <gxg-button type="icon-only" disabled=${boolean(labelDisabled, defaultValueDisabled)}><img slot="icon" src="${select(label, options, defaultValue)}"></gxg-button>
         `
         , {
         notes: {
@@ -57,7 +55,7 @@ stories.add('Primary - Text', () => `
         }
 })
 .add('Secondary - Icon', () => `
-        <gxg-button type="icon-only-secondary" disabled=${boolean(labelDisabled, defaultValueDisabled)}><img slot="icon" src="${select(label, options, defaultValue)}" alt="icon"></gxg-button>
+        <gxg-button type="icon-only-secondary" disabled=${boolean(labelDisabled, defaultValueDisabled)}><img slot="icon" src="${select(label, options, defaultValue)}"></gxg-button>
         `, {
         notes: {
                markdown: readme
