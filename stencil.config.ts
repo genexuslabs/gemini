53c1c521eef368ce12ed7e0fb8c0ff443e01fe71
Nicolás Cardelino <ncardeli@gmail.com>
2019-11-04 11:54:46 -0300

    Rename package to @genexus/gemini
    Also, rename dist files

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,36 +1,34 @@
-import { Config } from '@stencil/core';
-import { sass } from '@stencil/sass';
+import { Config } from "@stencil/core";
+import { sass } from "@stencil/sass";
 
 export const config: Config = {
-  namespace: 'gx-geminis-ds',
+  namespace: "gemini",
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader'
+      type: "dist",
+      esmLoaderPath: "../loader"
     },
     {
-      type: 'docs-readme'
+      type: "docs-readme"
     },
     {
-      type: 'www',
+      type: "www",
       serviceWorker: null, // disable service workers
-      copy: [
-        { src: 'assets' }
-      ]
+      copy: [{ src: "assets" }]
     }
   ],
   //globalStyle: 'src/globals/global.css',
   plugins: [
     sass({
       injectGlobalPaths: [
-        'src/globals/general.scss',
-        'src/globals/normalize.scss',
-        'src/globals/tokens-borders.scss',
-        'src/globals/tokens-colors.scss',
-        'src/globals/tokens-fonts.scss',
-        'src/globals/tokens-icons.scss',
-        'src/globals/tokens-outlines.scss',
-        'src/globals/tokens-spacing.scss'
+        "src/globals/general.scss",
+        "src/globals/normalize.scss",
+        "src/globals/tokens-borders.scss",
+        "src/globals/tokens-colors.scss",
+        "src/globals/tokens-fonts.scss",
+        "src/globals/tokens-icons.scss",
+        "src/globals/tokens-outlines.scss",
+        "src/globals/tokens-spacing.scss"
       ]
     })
   ]
