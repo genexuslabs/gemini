c6ddb0c7200c4f8d620b7527ff22891e98f263bf
bsastre <bsastre@genexus.com>
2020-05-27 18:16:02 -0300

    fix error

--- src/components/form-radio-group/form-radio-group.tsx
+++ src/components/form-radio-group/form-radio-group.tsx
@@ -38,53 +38,36 @@ export class FormRadioGroup {
 
   @Listen("keyPressed")
   keyPressedHandler(event: CustomEvent) {
-    const keyboardfocusableElements = document.querySelectorAll(
-      'a, button, input, textarea, select, details, [tabindex]:not([tabindex="-1"])'
-    );
-    console.log(keyboardfocusableElements);
-
-    if (event.detail.direction === "previous-tab") {
-      //tab key was pressed, set focus on the next element
-      const previousEl = this.el.previousElementSibling;
-    } else if (event.detail.direction === "next-tab") {
-      //tab and shift key were pressed, set focus on the previous element
-      const nextEl = this.el.nextElementSibling;
-    } else {
-      const currentActiveRadio = this.el.querySelector(
-        "gxg-form-radio[checked]"
-      );
-      currentActiveRadio.shadowRoot
-        .querySelector("input")
-        .setAttribute("tabindex", "-1");
-      currentActiveRadio.removeAttribute("checked");
-      if (event.detail.direction === "next") {
-        const nextSibling = currentActiveRadio.nextElementSibling;
-        if (nextSibling !== null) {
-          nextSibling.shadowRoot
-            .querySelector("input")
-            .setAttribute("tabindex", "0");
-          nextSibling.setAttribute("checked", "checked");
-          nextSibling.shadowRoot.querySelector("input").focus();
-        } else {
-          const firstRadio = this.el.querySelector(
-            "gxg-form-radio:first-child"
-          );
-          firstRadio.setAttribute("checked", "checked");
-          firstRadio.shadowRoot.querySelector("input").focus();
-        }
-      } else if (event.detail.direction === "previous") {
-        const prevSibling = currentActiveRadio.previousElementSibling;
-        if (prevSibling !== null) {
-          prevSibling.shadowRoot
-            .querySelector("input")
-            .setAttribute("tabindex", "0");
-          prevSibling.setAttribute("checked", "checked");
-          prevSibling.shadowRoot.querySelector("input").focus();
-        } else {
-          const lastRadio = this.el.querySelector("gxg-form-radio:last-child");
-          lastRadio.setAttribute("checked", "checked");
-          lastRadio.shadowRoot.querySelector("input").focus();
-        }
+    const currentActiveRadio = this.el.querySelector("gxg-form-radio[checked]");
+    currentActiveRadio.shadowRoot
+      .querySelector("input")
+      .setAttribute("tabindex", "-1");
+    currentActiveRadio.removeAttribute("checked");
+    if (event.detail.direction === "next") {
+      const nextSibling = currentActiveRadio.nextElementSibling;
+      if (nextSibling !== null) {
+        nextSibling.shadowRoot
+          .querySelector("input")
+          .setAttribute("tabindex", "0");
+        nextSibling.setAttribute("checked", "checked");
+        nextSibling.shadowRoot.querySelector("input").focus();
+      } else {
+        const firstRadio = this.el.querySelector("gxg-form-radio:first-child");
+        firstRadio.setAttribute("checked", "checked");
+        firstRadio.shadowRoot.querySelector("input").focus();
+      }
+    } else if (event.detail.direction === "previous") {
+      const prevSibling = currentActiveRadio.previousElementSibling;
+      if (prevSibling !== null) {
+        prevSibling.shadowRoot
+          .querySelector("input")
+          .setAttribute("tabindex", "0");
+        prevSibling.setAttribute("checked", "checked");
+        prevSibling.shadowRoot.querySelector("input").focus();
+      } else {
+        const lastRadio = this.el.querySelector("gxg-form-radio:last-child");
+        lastRadio.setAttribute("checked", "checked");
+        lastRadio.shadowRoot.querySelector("input").focus();
       }
     }
   }
