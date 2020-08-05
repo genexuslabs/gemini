a4c3541a5cdd958879ddb48522a3356a027ac923
bsastre <bsastre@genexus.com>
2020-08-05 18:53:57 -0300

    remove margin from all titles

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -3,10 +3,3 @@ import "@storybook/addon-storysource/register";
 import "@storybook/addon-knobs/register";
 import "storybook-dark-mode/register";
 import "@storybook/addon-viewport/register";
-
-import { addons } from "@storybook/addons";
-import geminiTheme from "./geminiTheme";
-
-addons.setConfig({
-  theme: geminiTheme
-});
