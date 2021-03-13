5548c2d99de1091d8b2487ab04b6d6ae0731ed19
Bruno Sastre <bsastre@genexus.com>
2021-03-12 22:44:39 -0300

    fix unnamed component

--- src/components/gxg-unnamed/gxg-unnamed.tsx
+++ src/components/gxg-unnamed/gxg-unnamed.tsx
@@ -12,26 +12,19 @@ export class GxgUnnamed {
 
   componentWillLoad() {
     this.itemsNodeList = this.el.querySelectorAll("gxg-unnamed-item");
-    console.log("this.itemsNodeList");
-    console.log(this.itemsNodeList);
   }
 
   onInputGxgformText(e) {
     const filterValue = e.detail.toLowerCase();
-    console.log(filterValue);
     this.itemsNodeList.forEach(item => {
       const itemHtmlElement = item as HTMLElement;
       const itemContent = itemHtmlElement.innerText.toLowerCase();
       if (!itemContent.includes(filterValue)) {
         itemHtmlElement.classList.add("opacity-zero");
-        setTimeout(function() {
-          itemHtmlElement.classList.add("height-zero");
-        }, 150);
+        itemHtmlElement.classList.add("height-zero");
       } else {
         itemHtmlElement.classList.remove("height-zero");
-        setTimeout(function() {
-          itemHtmlElement.classList.remove("opacity-zero");
-        }, 150);
+        itemHtmlElement.classList.remove("opacity-zero");
       }
       if (itemContent === filterValue) {
         itemHtmlElement.classList.add("exact-match");
