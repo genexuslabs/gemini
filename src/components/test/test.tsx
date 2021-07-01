ccad09dd12fd3cf394396f0e106d58b1bd34ce12
Bruno <brsastre@gmail.com>
2021-07-01 11:08:28 -0300

    changes before splitter modification

--- src/components/test/test.tsx
+++ src/components/test/test.tsx
@@ -1,15 +1,42 @@
-import { Component, h, Host } from "@stencil/core";
+// import { Component, h, Host, getMode } from "@stencil/core";
+
+// /**
+//  * @virtualProp {string} colormode - The mode determines which platform styles to use.
+//  */
+
+// @Component({
+//   tag: "gxg-test",
+//   styleUrl: "test.scss",
+//   shadow: true,
+// })
+// export class GxgTest {
+//   private mode = getMode(this);
+
+//   componentWillLoad() {
+//     console.log(this.mode);
+//   }
+
+//   render() {
+//     //return <Host mode={getMode(this)}></Host>;
+//     return <Host>{this.mode}</Host>;
+//   }
+// }
+
+import { Component, h, Host, getMode, State } from "@stencil/core";
 @Component({
   tag: "gxg-test",
   styleUrls: {
-    ios: "test.ios.scss",
-    md: "test.md.scss",
-    big: "test.big.scss",
+    regular: "test.scss",
+    large: "test.scss",
   },
   shadow: true,
 })
 export class GxgTest {
+  @State() private mode = getMode(this);
+  componentDidLoad() {
+    this.mode = getMode(this);
+  }
   render() {
-    return <Host>hola</Host>;
+    return <Host mode={this.mode}>{this.mode}</Host>;
   }
 }
