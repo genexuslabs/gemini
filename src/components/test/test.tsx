f543f26e4f2bef9122d29457b43c647a5bb46427
Bruno Sastre <bsastre@genexus.com>
2023-08-04 15:34:06 -0300

    remove icons.scss

--- src/components/test/test.tsx
+++ src/components/test/test.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Host, Prop } from "@stencil/core";
+import { Component, h, Host, Prop, Listen } from "@stencil/core";
 //import { GxgComboBox } from "../combo-box/combo-box";
 //import { GxgFormSelect } from "../form-select/gxg-select";
 //import { GxgModal } from "../modal/modal";
@@ -13,10 +13,36 @@ export class GxgTest {
   gxgTree: HTMLGxgTreeElement;
   @Prop() model;
 
+  tree1!: HTMLGxgTreeElement;
+  tree2!: HTMLGxgTreeElement;
+
+  componentDidLoad() {
+    this.tree1.addEventListener(
+      "checkboxClickedEvent",
+      function (e: CustomEvent) {
+        console.log("tree1 received the checkboxClickedEvent event", e.detail);
+      }
+    );
+    this.tree2.addEventListener(
+      "checkboxClickedEvent",
+      function (e: CustomEvent) {
+        console.log("tree1 received the checkboxClickedEvent event", e.detail);
+      }
+    );
+  }
+
   render() {
     return (
       <Host>
-        <gxg-tree model={this.model}></gxg-tree>
+        <gxg-tree
+          model={this.model}
+          ref={(el) => (this.tree1 = el as HTMLGxgTreeElement)}
+        ></gxg-tree>
+        <hr />
+        <gxg-tree
+          model={this.model}
+          ref={(el) => (this.tree2 = el as HTMLGxgTreeElement)}
+        ></gxg-tree>
       </Host>
     );
   }
