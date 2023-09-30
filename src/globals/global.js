47b9fcaf15178082ff6dbbb07c729b75c2a78efc
Bruno Sastre <54677414+bsastregx@users.noreply.github.com>
2023-09-30 17:52:30 -0300

    update gemini ch-styles

--- src/globals/global.js
+++ src/globals/global.js
@@ -1,4 +1,7 @@
-const head = document.head || document.getElementsByTagName("head")[0];
-const chStyle = document.createElement("ch-style");
-chStyle.setAttribute("href", "/globals/ch-styles.css");
-head.appendChild(chStyle);
+export default function () {
+  // or export default async function()
+  const head = document.head || document.getElementsByTagName("head")[0];
+  const chStyle = document.createElement("ch-style");
+  chStyle.setAttribute("href", "/globals/ch-styles.css");
+  head.appendChild(chStyle);
+}
