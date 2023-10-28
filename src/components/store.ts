88f1bb7eab78d227f5fdd551865771e1a15c7209
Bruno Sastre <54677414+bsastregx@users.noreply.github.com>
2023-10-28 19:22:36 -0300

    update gemini tokens

--- src/components/store.ts
+++ src/components/store.ts
@@ -2,6 +2,7 @@ import { createStore } from "@stencil/store";
 
 const { state } = createStore({
   large: document.documentElement.classList.contains("gxg-large"),
+  mercury: document.documentElement.classList.contains("mercury"),
   rtl: document.documentElement.getAttribute("dir") === "rtl",
 });
 
