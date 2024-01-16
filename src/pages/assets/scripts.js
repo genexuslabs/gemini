c91218b1051a80dfe3c698df269fc16aa171791d
Bruno Sastre <bsastre@genexus.com>
2024-01-16 12:22:50 +0100

    update comparator - add buttons

--- src/pages/assets/scripts.js
+++ src/pages/assets/scripts.js
@@ -27,27 +27,34 @@ document.addEventListener("DOMContentLoaded", function () {
   //2. Render navbar with buttons
   const navBar = document.createElement("nav");
   navBar.classList.add("navbar");
-  /*gemini button*/
-  const geminiButton = document.createElement("button");
-  geminiButton.innerText = "Set Gemini";
-  geminiButton.addEventListener("click", () => {
-    html.classList.remove("mercury");
-  });
-  /*mercury button*/
-  const mercuryButton = document.createElement("button");
-  mercuryButton.innerText = "Set Mercury";
-  mercuryButton.addEventListener("click", () => {
-    html.classList.add("mercury");
-  });
+
+  const mercuryOnly = document.body.hasAttribute("mercury");
+  console.log(mercuryOnly);
+  if (!mercuryOnly) {
+    /*gemini button*/
+    const geminiButton = document.createElement("button");
+    geminiButton.innerText = "Set Gemini";
+    geminiButton.addEventListener("click", () => {
+      html.classList.remove("mercury");
+    });
+    navBar.appendChild(geminiButton);
+    /*mercury button*/
+    const mercuryButton = document.createElement("button");
+    mercuryButton.innerText = "Set Mercury";
+    mercuryButton.addEventListener("click", () => {
+      html.classList.add("mercury");
+    });
+    navBar.appendChild(mercuryButton);
+  }
+
   /*toggle dark*/
   const toggleDarkButton = document.createElement("button");
   toggleDarkButton.innerText = "Toggle Dark";
   toggleDarkButton.addEventListener("click", () => {
     html.classList.toggle("dark");
   });
-  navBar.appendChild(geminiButton);
-  navBar.appendChild(mercuryButton);
   navBar.appendChild(toggleDarkButton);
+
   body.appendChild(navBar);
   html.classList.add("has-navbar");
 });
